cfd654cbee62d939216922be4555c9e6c0c44fc6
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-19 19:56:54 -0400

    Carpeta Data y Clase ConexiónBDD creadas

---
+++ Interfaces - Pequeño Sistema CRUD/Data/ConexiónBDD.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Interfaces___Pequeño_Sistema_CRUD.Data
+{
+    public class ConexiónBDD
+    {
+        private string CadenadeConexión = @"Server=LAPTOP-9G07MQQC\SQLEXPRESS;Database=SistemaCRUD;Trusted_Connection=True;";
+        public SqlConnection GetConnection() 
+        {
+            return new SqlConnection(CadenadeConexión);
+        }
+    }
+}

d30fb9ec4eee58847ca03575dc8703d768c560eb
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-19 20:13:01 -0400

    Interfaz con los métodos asignados realizada en Carpeta Repositories

---
+++ Interfaces - Pequeño Sistema CRUD/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces___Pequeño_Sistema_CRUD.Repositories
+{
+    //Nota: La <T> significa que estas acciones de la interfaz sirven para
+    //cualquier tipo de dato que le pasemos o necestemos para realizar el programa
+    //(ya sea int, string, decimal, etc.)
+    public interface IRepository<T>
+    {
+        void Save(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        T Search(int id);
+        List<T> GetAll();
+    }
+}

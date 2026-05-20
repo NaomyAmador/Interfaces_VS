3a19a696d8abc59393529760ba4fb5648b9e8693
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-19 20:01:23 -0400

    Carpeta Entidades creada y Clase Producto hecha

---
+++ Interfaces - Pequeño Sistema CRUD/Entidades/Producto.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces___Pequeño_Sistema_CRUD.Entidades
+{
+    public class Producto
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}

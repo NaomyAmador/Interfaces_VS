16dc403f5f32b554dc0b40e2e8e26222b606f346
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-19 20:06:37 -0400

    Clase Cliente en Entidades hecha

---
+++ Interfaces - Pequeño Sistema CRUD/Entidades/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces___Pequeño_Sistema_CRUD.Entidades
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nombre { get; set; }
+        public int Correo { get; set; }
+    }
+}
